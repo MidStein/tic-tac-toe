59c3983b7294d4a47a3b2b99f97a3fc542463ee2
MidStein <deepakchauhan19feb@gmail.com>
2022-06-22 20:28:46 +0530

    added menu for player names, restart and announcing victory

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const Gameboard = (function() {
-    const boardArr = ['', '', '', '', '', '', '', '', ''];
+    const boardArr = [];
     const getBoardAtIndex = function(index) {
         return boardArr[index];
     }
@@ -45,6 +45,9 @@ const Player = function(plName, plMark) {
 };
 const DisplayController = (function() {
     let spots, turn = 0;
+    let player;
+    const boardContainer = document.querySelector('.board-container');
+    const lines = document.querySelectorAll('.line');
     const getNameFromMark = function(mark) {
         for (let i = 0; i < player.length; i++) {
             if (player[i].getPlMark() == mark) {
@@ -64,17 +67,30 @@ const DisplayController = (function() {
             Gameboard.setBoardAtIndex(this.getAttribute('data-key'), 'O');
         }
         if (turn >= 4) {
+            let isGameOver = false;
             const is3InRow = Gameboard.check3InRow();
             if (is3InRow) {
-                console.log(`${getNameFromMark(is3InRow)} wins.`);;
+                document.querySelector('.winner-congo').textContent = `Congratulations to ${getNameFromMark(is3InRow)} for winning the game`;
+                isGameOver = true;
             } else if (Gameboard.checkDraw()) {
-                console.log('draw');
+                document.querySelector('.winner-congo').textContent = `Draw`;
+                isGameOver = true;
+            }
+            if (isGameOver) {
+                document.querySelector('.menu').style.visibility = 'visible';
+                document.querySelector('.winner-congo').style.visibility = 'visible';
+                window.setTimeout(() => {
+                    boardContainer.style.visibility = 'hidden';
+                    spots.forEach(spot => spot.textContent = '');
+                }, 1000);
+                lines.forEach(line => line.style.backgroundColor = '#4ade80');
+                pl1Name = document.querySelector('input#pl1Name').value = '';
+                pl2Name = document.querySelector('input#pl2Name').value = '';
             }
         }
         turn++;
     };
-    const renderBoard = function(board) {
-        const boardContainer = document.querySelector('.board-container');
+    const renderBoard = function() {
         for (let i = 0; i < 9; i++) {
             let spot = document.createElement('div');
             spot.setAttribute('data-key', i);
@@ -84,9 +100,24 @@ const DisplayController = (function() {
         spots = document.querySelectorAll('.spot');
         spots.forEach(spot => spot.addEventListener('click', playerClicked));
     };
-    return {
-        renderBoard
+    const startGame = function() {
+        document.querySelector('.winner-congo').style.visibility = 'hidden';
+        start.textContent = 'Restart Game';
+        document.querySelector('.menu').style.visibility = 'hidden';
+        lines.forEach(line => line.style.backgroundColor = '#000');
+        for (let i = 0; i < 9; i++) {
+            Gameboard.setBoardAtIndex(i, '');
+        }
+        boardContainer.style.visibility = 'visible';
+        const pl1Name = document.querySelector('input#pl1Name').value;
+        const pl2Name = document.querySelector('input#pl2Name').value;
+        player = [Player(pl1Name, 'X'), Player(pl2Name, 'O')];
+        turn = 0;
+        renderBoard();
     };
+    return {
+        startGame
+    }
 })();
-const player = [Player('Felix', 'X'), Player('Sean', 'O')];
-DisplayController.renderBoard([]);
+const start = document.querySelector('.menu > button');
+start.addEventListener('click', DisplayController.startGame);
